4ded05e97ecb5f0e31122ba43af95da51a975cf5
David Hin <david.hin@lifelenz.com>
2025-03-07 10:02:19 +1030

    Windows and openings

--- dwell.py
+++ dwell.py
@@ -11,6 +11,7 @@ class Dwell:
         self.depth = 0
         self.height = 0
         self.thickness = 0
+        self.wall_objects = []
 
     def create_floor(self, vertices):
         self.vertices = vertices
@@ -18,7 +19,7 @@ class Dwell:
         floor = bpy.data.objects.new("Floor", mesh)
         bpy.context.collection.objects.link(floor)
         verts = [(x, y, 0) for (x, y) in vertices]
-        face = [i for i in range(len(verts))]
+        face = list(range(len(verts)))
         mesh.from_pydata(verts, [], [face])
         mesh.update()
         xs = [v[0] for v in verts]
@@ -29,6 +30,7 @@ class Dwell:
     def create_walls(self, height, thickness):
         self.height = height
         self.thickness = thickness
+        self.wall_objects = []
         for i in range(len(self.vertices)):
             v1 = self.vertices[i]
             v2 = self.vertices[(i + 1) % len(self.vertices)]
@@ -43,6 +45,7 @@ class Dwell:
             wall.scale.x = length / 2
             wall.scale.y = thickness / 2
             wall.scale.z = height / 2
+            self.wall_objects.append(wall)
 
     def add_window(self, edge_index, window_width, window_height, sill, offset=0):
         v1 = self.vertices[edge_index]
@@ -65,6 +68,33 @@ class Dwell:
         window = bpy.context.active_object
         window.scale.x = window_width / 2
         window.scale.y = window_height / 2
+
+    def add_opening(self, edge_index, opening_width, opening_height, sill=0, offset=0):
+        v1 = self.vertices[edge_index]
+        v2 = self.vertices[(edge_index + 1) % len(self.vertices)]
+        length = math.sqrt((v2[0] - v1[0]) ** 2 + (v2[1] - v1[1]) ** 2)
+        angle = math.atan2(v2[1] - v1[1], v2[0] - v1[0])
+        mid_x = (v1[0] + v2[0]) / 2
+        mid_y = (v1[1] + v2[1]) / 2
+        ux = (v2[0] - v1[0]) / length
+        uy = (v2[1] - v1[1]) / length
+        cx = mid_x + offset * ux
+        cy = mid_y + offset * uy
+        opening_center = (cx, cy, sill + opening_height / 2)
+        bpy.ops.mesh.primitive_cube_add(
+            size=2, location=opening_center, rotation=(0, 0, angle)
+        )
+        opening_obj = bpy.context.active_object
+        opening_obj.scale.x = opening_width / 2
+        opening_obj.scale.y = self.thickness / 2 * 1.1
+        opening_obj.scale.z = opening_height / 2
+        wall = self.wall_objects[edge_index]
+        mod = wall.modifiers.new(name="OpeningCutout", type="BOOLEAN")
+        mod.operation = "DIFFERENCE"
+        mod.object = opening_obj
+        bpy.context.view_layer.objects.active = wall
+        bpy.ops.object.modifier_apply(modifier=mod.name)
+        bpy.data.objects.remove(opening_obj, do_unlink=True)
 
     def build(self, filepath):
         if os.path.exists(filepath):
--- main.py
+++ main.py
@@ -9,5 +9,8 @@ design = Dwell()
 design.create_floor([(0, 0), (10, 0), (10, 6), (6, 6), (6, 10), (0, 10)])
 design.create_walls(3, 0.2)
 design.add_window(edge_index=4, window_width=2, window_height=1.5, sill=1, offset=0)
+design.add_window(edge_index=0, window_width=2, window_height=1.5, sill=1, offset=0)
+design.add_window(edge_index=0, window_width=2, window_height=1.5, sill=1, offset=0)
+design.add_opening(edge_index=1, opening_width=2, opening_height=2, sill=0, offset=0)
 
 design.build("output.blend")
